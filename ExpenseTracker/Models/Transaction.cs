a898c9e513cb40dca021d24edfdb3b01e4885c8a
ChoiYingY <choiying.yau@stonybrook.edu>
2024-03-23 16:31:57 -0400

    set up base

---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+
+        [Key]
+        public int TransactionId { get; set; }
+
+        // CategoryId (foreign key)
+        public int CategoryId {  get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        // Create column in sql table, which can be null or have up to 75 chars
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date {  get; set; } = DateTime.Now;
+    }
+}
